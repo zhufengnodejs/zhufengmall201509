7e40761a846a4654586a0dcb324a3556b3efece0
zfpx <zfpx@126.com>
2016-01-31 13:16:49 +0800

    配置package.json

--- admin/webpack.config.js
+++ admin/webpack.config.js
@@ -2,45 +2,47 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  entry:'./app.js',
+  entry:['./app.js'],
   output:{
      path:path.resolve(__dirname,'dist'),
      filename:'bundle.js'
   },
   module:{
-      loaders:[
+      loaders: [
           {
-              test:/\.jade$/,
-              loader:'jade',
-              exclude:/node_modules/
+              test: /\.jade$/,
+              loader: 'jade',
+              exclude: /node_modules/
           },
           {
-              test:/\.css$/,
-              loader:'style!css',
-              exclude:/node_modules/
+              test: /\.css$/,
+              loader: "style!css" ,
+              exclude: /node_modules/
           },
           {
-              test:/\.js$/,
-              loader:'babel',
-              query:{
-                  presets:['es2015']
+              loader: 'babel-loader',
+              exclude: /node_modules/,
+              test: /\.js?$/,
+              query: {
+                  presets: ['es2015'],
               },
-              exclude:/node_modules/
           },
           {
-              test:/\.less/,
-              loader:'style!css!less',
-              exclude:/node_modules/
+              test: /\.less$/,
+              exclude: /node_modules/,
+              loader: "style!css!less?strictMath&noIeCompat"
           },
           {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
           {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
           {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
           {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
-
       ]
   },
-  resolve:{
-     root:['./src'],
-     extensions:['','js','css']
-  }
+
+    resolve: {
+        root: [
+            path.resolve('./src') //指定根路径
+        ],
+        extensions:['','.js','.css']
+    }
 }
